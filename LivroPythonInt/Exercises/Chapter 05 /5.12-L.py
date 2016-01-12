38a9ce8cb5558f2a73060243d458ea92d91bf945
Leonardo Kiyota <leonardo.kiyota@gmail.com>
2016-01-11 23:36:30 -0200

    Arrumando a casa

---
+++ LivroPythonInt/Exercises/Chapter 05 /5.12-L.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+x = 1
+soma = 0
+
+while x <= 5:
+    n = int(input("{} Digite o número: ".format(x)))
+    soma = soma + n
+    x = x + 1
+
+print("Média: {:.2f}".format(5/2))
